8c573b6b6f6b31821d5ccfc0d8ae84d023044e15
Giuseppe <giusepperaffa1988@gmail.com>
2021-03-01 15:29:30 +0100

    Database + immagini
    init

---
+++ db.php
@@ -0,0 +1,52 @@
+<?php 
+
+dischi= [
+    [
+        'album' => '24H',
+        'autore' => 'Tom Jones',
+        'anno' => '2010'
+    ],
+    [
+        'album' => 'Adriano',
+        'autore' => 'Adriano Celentano',
+        'anno' => '2020'
+    ],
+    [
+        'album' => 'Revolucion',
+        'autore' => 'Carlos Puebla',
+        'anno' =>  '1970'
+    ],
+    [
+        'album' =>  'Cuba',
+        'autore' => 'B V S C',
+        'anno' => '1980'
+    ],
+    [
+        'album' => 'Baila',
+        'autore' => 'Gipsy King',
+        'anno' => '2000'
+    ],
+    [
+        'album' => 'Daft',
+        'autore' => 'Daft Punk',
+        'anno' => '2005'
+    ],
+    [
+        'album' => 'Nuvole e pover',
+        'autore' => 'Enzo Avitabile',
+        'anno' => '2011'
+    ],
+    [
+        'album' => 'Fred GH',
+        'autore' => 'Fred Buscaglione',
+        'anno' => '1960'
+    ],
+    [
+        'album' => 'Bud & Terence'
+        'autore' => 'Oliver Onions'
+        'anno' => '1978'
+    ],
+
+];
+
+?>
