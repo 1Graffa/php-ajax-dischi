656c1fa1c04b67ce2e02f34b273082b80fb092d4
Giuseppe <giusepperaffa1988@gmail.com>
2021-03-01 15:37:25 +0100

    correzioni su DB

--- db.php
+++ db.php
@@ -4,48 +4,57 @@ dischi= [
     [
         'album' => '24H',
         'autore' => 'Tom Jones',
-        'anno' => '2010'
+        'anno' => '2010',
+        'img' => "img/tom.jpg"
     ],
     [
         'album' => 'Adriano',
         'autore' => 'Adriano Celentano',
-        'anno' => '2020'
+        'anno' => '2020',
+        'img' => "img/adri.jpg"
     ],
     [
         'album' => 'Revolucion',
         'autore' => 'Carlos Puebla',
-        'anno' =>  '1970'
+        'anno' =>  '1970',
+        'img' => "img/carlos.jpg"
     ],
     [
         'album' =>  'Cuba',
         'autore' => 'B V S C',
-        'anno' => '1980'
+        'anno' => '1980',
+        'img' => "img/buena.jpg"
     ],
     [
-        'album' => 'Baila',
-        'autore' => 'Gipsy King',
-        'anno' => '2000'
+        'album' => 'Spaghetti Western',
+        'autore' => 'Ennio Morricone',
+        'anno' => '2000',
+        'img' => "img/ennio.jpg"
     ],
     [
-        'album' => 'Daft',
-        'autore' => 'Daft Punk',
-        'anno' => '2005'
+        'album' => "Gigi Dag 90'",
+        'autore' => "Gigi d'agostino",
+        'anno' => '2005',
+        'img' => 
     ],
     [
         'album' => 'Nuvole e pover',
         'autore' => 'Enzo Avitabile',
-        'anno' => '2011'
+        'anno' => '2011',
+        'img' => "img/enzo.jpg"
     ],
     [
         'album' => 'Fred GH',
         'autore' => 'Fred Buscaglione',
-        'anno' => '1960'
+        'anno' => '1960',
+        'img' => 
     ],
     [
         'album' => 'Bud & Terence'
         'autore' => 'Oliver Onions'
-        'anno' => '1978'
-    ],
+        'anno' => '1978',
+        'img' => "img/bud.jpg"
+    ]
 
 ];
 
